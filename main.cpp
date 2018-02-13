f091089d512b912ae4ab1b6b9fb0e8bb20b7d21c
rpersons1 <rpersons1@mail.csuchico.edu>
2018-02-13 12:02:13 -0800

    i added nickname

--- main.cpp
+++ main.cpp
@@ -7,13 +7,21 @@ using namespace std;
 int main()
 {
   //declare variables
-  string first, last;
+  string first, last, nickname;
+   
   //get user input
   cout<<"What is your first name?\n";
   cin>>first;
   cout<<"What is your last name?\n";
   cin>>last;
+
   //tell fortune
+  nickname.push_back(first[0]);
+  nickname.push_back('.');
+  nickname.push_back(last[0]);
+  nickname.push_back('.');
+
+  cout<<"Welcome, "<<nickname<<", here is  your fortune...\n";
 
   return 0;
 }
