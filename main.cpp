118df4fd4e518a2b32898084adaaa809cf5269e8
rpersons1 <rpersons1@mail.csuchico.edu>
2018-02-13 11:22:36 -0800

    my version asks for you first and last name

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors:Ryan Persons
 #include<iostream>
 #include<string>
 
@@ -7,9 +7,12 @@ using namespace std;
 int main()
 {
   //declare variables
-
+  string first, last;
   //get user input
-
+  cout<<"What is your first name?\n";
+  cin>>first;
+  cout<<"What is your last name?\n";
+  cin>>last;
   //tell fortune
 
   return 0;
