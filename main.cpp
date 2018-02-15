2b59d1b53c7ce87f98e95d95e12f9c805c2d834f
rpersons1 <rpersons1@mail.csuchico.edu>
2018-02-15 11:39:36 -0800

    tells your fortune

--- main.cpp
+++ main.cpp
@@ -7,21 +7,24 @@ using namespace std;
 int main()
 {
   //declare variables
-  string first, last, nickname;
-   
+  string first, last, nickname, word;
+  int lucky;
+
   //get user input
   cout<<"What is your first name?\n";
   cin>>first;
   cout<<"What is your last name?\n";
   cin>>last;
 
-  //tell fortune
   nickname.push_back(first[0]);
   nickname.push_back('.');
   nickname.push_back(last[0]);
   nickname.push_back('.');
-
   cout<<"Welcome, "<<nickname<<", here is  your fortune...\n";
+
+  //tell fortune
+  lucky = first.length(); 
+  cout<<"Your lucky number is "<<lucky<<endl; 
 
   return 0;
 }
