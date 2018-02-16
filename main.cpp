3d34f8b57ac7ee02f52c6bc5a86a8f6ae857fa56
rpersons1 <rpersons1@mail.csuchico.edu>
2018-02-15 17:49:56 -0800

    added to if statments

--- main.cpp
+++ main.cpp
@@ -25,6 +25,25 @@ int main()
   //tell fortune
   lucky = first.length(); 
   cout<<"Your lucky number is "<<lucky<<endl; 
+  word = first;
+  char letter = word[0];
+  if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' 
+  || letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')
+  {
+  cout<<"You are destined to be famous!\n";
+  }
+  else
+  {
+  cout<<"You should keep a low profile.\n";
+  }
+    
+  char let = last[0];
+  if(let == 'a' || let == 'e' || let == 'i' || let == 'o' || let == 'u'
+  || let == 'A' || let == 'E' || let == 'I' || let == 'O' || let == 'U')
+  {
+  cout<<"You have already met your true love.\n";
+  }
 
+  cout<<"Have a good day.\n";
   return 0;
 }
