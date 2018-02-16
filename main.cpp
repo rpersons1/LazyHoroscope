6572c2d5d454d153668d78c477aa431ebb139c9c
rpersons1 <rpersons1@mail.csuchico.edu>
2018-02-15 19:05:10 -0800

    fixed error

--- main.cpp
+++ main.cpp
@@ -20,30 +20,33 @@ int main()
   nickname.push_back('.');
   nickname.push_back(last[0]);
   nickname.push_back('.');
-  cout<<"Welcome, "<<nickname<<", here is  your fortune...\n";
+  cout<<"Welcome, "<<nickname<<", here is your fortune...\n";
 
   //tell fortune
   lucky = first.length(); 
-  cout<<"Your lucky number is "<<lucky<<endl; 
+  cout<<"your lucky number is "<<lucky<<endl; 
   word = first;
   char letter = word[0];
   if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' 
   || letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')
   {
-  cout<<"You are destined to be famous!\n";
+  cout<<"you are destined to be famous!\n";
   }
   else
   {
-  cout<<"You should keep a low profile.\n";
+  cout<<"you should keep a low profile.\n";
   }
     
   char let = last[0];
   if(let == 'a' || let == 'e' || let == 'i' || let == 'o' || let == 'u'
   || let == 'A' || let == 'E' || let == 'I' || let == 'O' || let == 'U')
   {
-  cout<<"You have already met your true love.\n";
+  }
+  else
+  {
+  cout<<"you have already met your true love.\n";
   }
 
-  cout<<"Have a good day.\n";
+  cout<<"have a good day!\n";
   return 0;
 }
